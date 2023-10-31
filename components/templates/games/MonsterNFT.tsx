341a0b6b7d52114a9bfd828b47a564b60b92bb10
David Boller <david@virtual-graphics.ch>
2023-10-31 14:57:20 +0100

    Added the NFT section

---
+++ components/templates/games/MonsterNFT.tsx
@@ -0,0 +1,77 @@
+
+import { 
+    
+    MediaRenderer, 
+    Web3Button, 
+    useActiveClaimCondition, 
+    useContract 
+
+
+} from "@thirdweb-dev/react";
+
+import { NFT } from "@thirdweb-dev/sdk";
+import { MONSTERS_ADDRESS } from "../../../const/addresses";
+import { ethers } from "ethers";
+import { useState } from "react";
+
+type Props = {
+    nft: NFT;
+};
+
+function MonsterNFT({ nft }: Props) {
+    const { contract } = useContract(MONSTERS_ADDRESS);
+    const { data, isLoading } = useActiveClaimCondition(
+        contract,
+        nft.metadata.id, // Token ID required for ERC1155 contracts here.
+      );
+
+      const [claimQuantity, setClaimQuantity] = useState(1);
+  const increment = () => {
+    if (claimQuantity < claimQuantity) {
+      setClaimQuantity(claimQuantity + 1);
+    }
+  };
+  const decrement = () => {
+    if (claimQuantity > 1) {
+      setClaimQuantity(claimQuantity - 1);
+    }
+  };
+
+    return (
+
+        <div key={nft.metadata.id} className='mx-auto justify-items-center py-12 border-white'>
+            <div className="rounded-3xl ">
+            <MediaRenderer 
+                src={nft.metadata.image}
+                height="100%"
+                width="100%"
+        
+            /></div>
+            <div className=" text-yellow-100 text-2xl text-bold  text-center  pt-4 ">{nft.metadata.name}</div>
+            {!isLoading && data ? (
+                <div className="text-white text-center py-2">Cost: {ethers.utils.formatEther(data?.price)}{" " + data?.currencyMetadata.symbol}</div>
+            ) :(
+                <div className='text-xl text-white'>Loading...</div>
+            )}
+
+<div className="py-4 flex items-center justify-center m-auto rounded-2xl border-none">
+
+
+<Web3Button
+                        contractAddress={MONSTERS_ADDRESS}
+                        action={(contract) => contract.erc1155.claim(nft.metadata.id, 1)}
+                     
+                        onSuccess={() => {
+                          
+                          alert("Successfully claimed NFTs");
+                        }}
+                      >
+                       Buy now
+                      </Web3Button>
+</div>
+
+        </div>
+    )
+};
+
+export default MonsterNFT
--- components/templates/shop/Shop.tsx
+++ components/templates/shop/Shop.tsx
@@ -1,22 +1,49 @@
-
-import {useState } from 'react'
 import type { NextPage } from "next";
+
+import {
+  MediaRenderer,
+  useActiveClaimConditionForWallet,
+  useAddress,
+  useClaimConditions,
+  useClaimedNFTSupply,
+  useClaimerProofs,
+  useClaimIneligibilityReasons,
+  useContract,
+  useContractMetadata,
+  useUnclaimedNFTSupply,
+  Web3Button,
+  useNFTs,
+} from "@thirdweb-dev/react";
+import { MONSTERS_ADDRESS } from "../../../const/addresses";
+import { NFT } from "@thirdweb-dev/sdk";
+import { BigNumber, utils } from "ethers";
+import { useState } from "react";
 import { FC, useEffect } from 'react';
-import Image from 'next/image'
+import { ethers } from "ethers";
+import Image from 'next/image';
+import { useRouter } from "next/router";
 import React from 'react';
-import {MediaRenderer, useContract, useContractMetadata, Web3Button} from "@thirdweb-dev/react";
+import MonsterNFT from "../games/MonsterNFT";
+
+import Link from "next/link";
+const myNftDropContractAddress = "0xD671735b14ddb2DA84433bf291f00355de068992";
+
+const NFTMint = () => {
+
+const { contract: nftDrop } = useContract(myNftDropContractAddress);
+const address = useAddress();
+const [quantity, setQuantity] = useState(1);
+const { data: contractMetadata } = useContractMetadata(nftDrop);
+const claimConditions = useClaimConditions(nftDrop);
+
+const { contract } = useContract(MONSTERS_ADDRESS);
+    const { data: nfts } = useNFTs(contract);
+    console.log(nfts);
+
+  return (
 
 
-
-const Shop = () => {
- 
-  const { contract: tokenDropContract} = useContract("0xd23342d614a1ff1d7bc84b9041C8615532D55C6D");
-  const [amountToClaim, setAmountToClaim] = useState("");
-  
-      return (
-
-
-        <div className="justify-center px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 lg:pt-6">
+    <div className="justify-center px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 lg:pt-6">
         <div className="max-w-screen-sm sm:text-center sm:mx-auto">
           
   
@@ -32,82 +59,70 @@ const Shop = () => {
           
             <hr className="w-full  border-yellow-200" />
             <p className="text-base font-semibold tracking-wide text-yellow-200 lg:text-2xl  md:text-lg sm:px-4 py-8 font-Acme">
-           Mint your SAVAGE VARAN NFT and go on a Treasure Hunt. 
+           Play the KAOS MOON Ritual Defi and the Card games and earn rewards. 
             </p>
 
-<div className="flex justify-center ">
 
 
 
+<div className="justify-center mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 lg:pt-10">
+    
 
-<div className="bg-black rounded-2xl w-3/4 drop-shadow-lg ">
-
-
-<Image
-              className="p-10 shadow-3xl"
-              src="/assets/pack_image.jpg"
-              alt="Beast Collection"
-              width={1500}
-              height={1500}
-            />
-
-<h1 className="text-yellow-300  text-center text-xl font-Amethysta">KAOS MOON: Beast Packs</h1>
-
-
-  
-<div className="mt-5 mb-7 max-w-sm justify-items-center">
-
-          
-    </div>
-   
-   
-    <div className='w-60 flex items-center justify-center m-auto pb-12'>
-    <Web3Button 
         
-         
-          contractAddress="0x94B0E73c4328A53ccc2CdeDbEeB2aF625984Af90"
-          action={(contract) => contract.erc20.claim(amountToClaim)}
-          onSuccess={() => alert("Claimed!")}
-          onError={(err) => alert(err)}
-        >
-          Claim NFT
-        
-          </Web3Button></div>
-         </div>
-              </div>
-</div>
- 
+       
+
+        <p className="text-base text-white lg:text-lg md:text-lg sm:px-4 pt-6 font-Jost">
+       Purchase MONSTER HUNT NFTs with $BLOOD to boost your Vampires power and gain more $BLOOD. 
+        </p>
+        </div>
+        </div>
+
+            {!nfts ? (
+                <div className="flex justify-center mx-auto p-24 ">
+                    
+                </div>
+            ) : (
+                <div className="grid grid-cols-3 gap-4 ">
+                    {nfts?.map((nftItem) => (
+                        <MonsterNFT
+                            key={nftItem.metadata.id}
+                            nft={nftItem}
+                        />
+                    ))}
+                </div>
+            )}
+
+
 <div className="pb-12  justify-items-center">
 
 <div className=" pt-14 m-auto  justify-center">
-<h1 className=" text-yellow-100 text-2xl text-center  font-Roboto">
-              Use your KAOS MOON NFTs for the following cool things:
+<h1 className=" text-yellow-100 text-2xl text-center  font-Jost">
+              Use your IMMORTAL COIL tokens for the following cool things:
             </h1></div>
 
       <div className="w-4/5 m-auto grid gap-8 row-gap-5 md:row-gap-8 lg:grid-cols-3 mt-10 sm:grid-cols-2">
         <div className="duration-300 transform bg-red-950/30 border-l-4 border-deep-purple-accent-900 hover:-translate-y-2 rounded-2xl">
           <div className="h-full p-5 rounded-r shadow-sm">
-            <h6 className="mb-2 leading-5 text-yellow-200 font-Acme">Earn rewards in the RPG</h6>
+            <h6 className="mb-2 leading-5 text-yellow-200 font-Jost">Earn rewards in the RPG</h6>
             <p className="text-sm text-white">
-              Get rewards for missions and quests successfully accomplished in the Stargazer RPG and trade tokens for in-game NFTs.
+              Get rewards for missions and quests successfully accomplished in the Memento Mori RPG and trade tokens for in-game NFTs.
             </p>
           </div>
         </div>
         <div className="duration-300 transform bg-red-950/30 border-l-4 border-deep-purple-accent-400 hover:-translate-y-2 rounded-2xl">
           <div className="h-full p-5  rounded-r shadow-sm">
-            <h6 className="mb-2  leading-5 text-yellow-200 font-Acme">Power up your Stargate</h6>
+            <h6 className="mb-2  leading-5 text-yellow-200 font-Jost">Boost your Vampire</h6>
             <p className="text-sm text-white">
-              Use the Stardust tokens to claim cosmic energy sources for your Stargate and get more STAR as a reward.
-            </p>
+              Use various monsters to boost the powers of your vampire and earn $BLOOD tokens in the BLoodhunt DeFi game.</p>
           </div>
         </div>
         <div className="duration-300 transform bg-red-950/30 border-l-4 border-deep-purple-accent-400 hover:-translate-y-2 rounded-2xl">
           <div className="h-full p-5  rounded-r shadow-sm">
-            <h6 className="mb-2 leading-5 text-yellow-200 font-Acme">Vote for your stories
+            <h6 className="mb-2 leading-5 text-yellow-200 font-Jost">Win reward NFTs
               
             </h6>
             <p className="text-sm text-white">
-              Use the tokens to participate in the anime and webtoons and get rewarded for a story path well-chosen.
+              Use the tokens to participate in the anime and webtoons and get rewarded for each correctly answered question.
             </p>
             
           </div>
@@ -115,18 +130,20 @@ const Shop = () => {
         </div>
 <div className="py-12 w-full justify-items-center mx-auto">
 
-  <h1 className="text-lg text-center text-yellow-100 font-Jost py-2 font-semibold">NFT contract address on Polyscan:</h1>
-            <p className="text-base text-center text-yellow-100 font-Jost px-2">
-              Kaos Moon Collection: 0x94B0E73c4328A53ccc2CdeDbEeB2aF625984Af90
+  <h1 className="text-lg text-center text-white font-Jost py-2">Contract Addresses on Bscscan:</h1>
+            <p className="text-sm text-center text-white font-Jost px-2">
+             Founders Edition NFTs: 0xD671735b14ddb2DA84433bf291f00355de068992
             </p>
-            </div> 
-      
+            <p className="text-sm text-center text-white font-Jost py-2">
+              Monster NFTs: 0x7623dCee1c3De24129CC15a8398F7d8731bDAccB
+            </p></div> 
+      </div>
  </div>
  
- </div>
 
-      );
-    };
 
- 
-  export default Shop;
+  );
+};
+
+
+export default NFTMint;
